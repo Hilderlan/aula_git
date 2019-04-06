d3f76384f35dc44c62790c80b1a8dd410e07030d
mtb <mateustitob@hotmail.com>
2019-04-06 15:10:00 -0300

    modified cliente

---
+++ Cliente.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+
+public class Cliente{
+private String nome;
+private String cpf;
+private ArrayList<Conta> listacontas;
+
+public Cliente(String nome,String cpf,ArrayList<Conta> listacontas){
+this.nome=nome;
+this.cpf=cpf;
+this.listacontas=listacontas;}
+
+public String getNome() {
+        return this.nome;
+    }
+
+public String getCpf() {
+        return this.cpf;
+    }
+
+public ArrayList<Conta> getListaContas() {
+        return this.listacontas;
+    }
+
+
+public void setNome(String nome) {
+        this.nome=nome;
+    }
+
+public void setCpf(String cpf) {
+        this.cpf=cpf;
+    }
+
+public void setListaContas(ArrayList<Conta> listacontas) {
+        this.listacontas=listacontas;
+    }
+
+public void saque(double value, Conta conta){
+Operacao opr = new Operacao(value,null,cont1);
+opr.saque();
+}
+
+public void deposito(double value, Conta conta){
+Operacao opr = new Operacao(value,null,cont1);
+opr.deposito();
+}
+
+public void transferencia(double value, Conta remetente,Conta destinatario){
+Operacao opr = new Operacao(value,remetente,destinatario);
+opr.transferencia();
+}
+
+public void excluirConta(int num_agencia) {
+  for conta : listacontas {
+    if (conta.getAgencia() == num_agencia){
+      listacontas.remove(conta);
+    }
+  }
+}
+
+
+}
