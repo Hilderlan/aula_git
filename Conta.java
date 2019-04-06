882393575f14bba1275c769876a93b52af114655
root <root@localhost.localdomain>
2019-04-06 14:22:38 -0300

    Criado a classe Conta

---
+++ Conta.java
@@ -0,0 +1,13 @@
+public class Conta {
+
+  private double saldo = 0;
+
+  private void setSaldo(double valor) {
+    this.saldo = valor;
+  }
+
+  private double getSaldo() {
+    return this.saldo;
+  }
+
+} // fim Conta
