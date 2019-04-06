e81d043153128702558956f9d0143be5043b9208
pvitoraluz <pedro_vitor_01@hotmail.com>
2019-04-06 14:18:03 -0300

    implementa cliente

--- Cliente.java
+++ Cliente.java
@@ -1,1 +1,35 @@
-teste teste teste
+import java.util.ArrayList;
+
+
+public class Cliente{
+private String nome;
+private String cpf;
+private ArrayList<Conta> listacontas;
+
+public Cliente(String nome,String cpf,ArrayList<Conta> listacontas){
+this.nome=nome;
+this.cpf=cpf;
+this.listacontas=listacontas;}
+
+public String getNome() {
+        return this.nome;
+    }
+
+public String getCpf() {
+        return this.cpf;
+    }
+ 
+public ArrayList<Conta> getListaContas() {
+        return this.listacontas;
+    }
+
+public void adicionarConta(Conta conta){
+this.getListaContas().add(conta);
+}
+
+public void operacao(){
+
+
+}
+
+}
