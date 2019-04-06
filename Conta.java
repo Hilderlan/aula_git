310f2b655b9ceb73871090adb79b6adb50622d8f
mtb <mateustitob@hotmail.com>
2019-04-06 14:57:13 -0300

    alterado

--- Conta.java
+++ Conta.java
@@ -1,5 +1,6 @@
 public class Conta {
 
+  private int num_agencia;
   private double saldo = 0;
 
   private void setSaldo(double valor) {
@@ -8,6 +9,10 @@ public class Conta {
 
   private double getSaldo() {
     return this.saldo;
+  }
+
+  private int getAgencia() {
+    return num_agencia;
   }
 
 } // fim Conta
