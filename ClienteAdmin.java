ac5fc7843f06077d0d6b3b874e0d038c17c90f2a
mtb <mateustitob@hotmail.com>
2019-04-07 10:47:12 -0300

    verifica e cadastra Cliente

--- ClienteAdmin.java
+++ ClienteAdmin.java
@@ -1,15 +1,34 @@
 import java.util.ArrayList;
 
-public class ClienteAdmin{
-private ArrayList<Cliente> listaclientes;
+public class ClienteAdmin {
 
+    private ArrayList<Cliente> listaclientes;
 
-public ArrayList<Cliente> getListaClientes(){
-return this.listaclientes;
-}
+    public ArrayList<Cliente> getListaClientes() {
+    return this.listaclientes;
+    }
 
-public void setListaClientes(ArrayList<Cliente> listaclientes) {
+    public void setListaClientes(ArrayList<Cliente> listaclientes) {
         this.listaclientes=listaclientes;
     }
 
-}
+    public boolean isCliente(String cpf) {
+        for (Cliente c : listaclientes) {
+            if (cpf == c.getCpf()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void cadastraCliente(Cliente cliente) {
+        if (isCliente(cliente.getCpf()) == true) {
+            listaclientes.add(cliente);
+            System.out.println("\nCliente cadastrado.");
+        }
+        else {
+            System.out.println("\nCliente já existente.");
+        }
+    }
+
+} // fim Adm clientes
