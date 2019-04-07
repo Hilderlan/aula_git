b4081cd29a22d5a58c3acbb2f447855652163ad8
pvitoraluz <pedro_vitor_01@hotmail.com>
2019-04-07 09:20:11 -0300

    Criada Classe administradora de clientes

---
+++ ClienteAdmin.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ClienteAdmin{
+private ArrayList<Cliente> listaclientes;
+
+
+public ArrayList<Cliente> getListaClientes(){
+return this.listaclientes;
+}
+
+public void setListaClientes(ArrayList<Cliente> listaclientes) {
+        this.listaclientes=listaclientes;
+    }
+
+}
